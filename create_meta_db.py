ecd8581c7c8034fc0cd5b7faadc1d7558ae9e97c
j-walheim <jonas.walheim@protonmail.ch>
2024-09-23 21:57:22 +0200

    use all sample metadata

--- create_meta_db.py
+++ create_meta_db.py
@@ -3,6 +3,9 @@ import xml.etree.ElementTree as ET
 import duckdb
 
 # Connect to DuckDB
+#overwrite the database if it already exists
+if os.path.exists('gse_metadata.db'):
+    os.remove('gse_metadata.db')
 con = duckdb.connect('gse_metadata.db')
 
 # Create a table to store the GSE metadata
@@ -32,30 +35,57 @@ def extract_metadata(xml_file):
     
     namespace = {'ns': 'http://www.ncbi.nlm.nih.gov/geo/info/MINiML'}
     
-    def find_text(path):
-        element = root.find(path, namespaces=namespace)
-        return element.text.strip() if element is not None else ''
+    def find_text(path, element=root):
+        return '; '.join(set(e.text.strip() for e in element.findall(path, namespaces=namespace) if e.text))
 
     series = root.find('.//ns:Series', namespaces=namespace)
-    sample = root.find('.//ns:Sample', namespaces=namespace)
+    samples = root.findall('.//ns:Sample', namespaces=namespace)
     
+    sample_data = {
+        'organism': set(),
+        'treatment': set(),
+        'treatment_protocol': set(),
+        'source': set(),
+        'characteristics': set(),
+        'molecule': set(),
+        'extract_protocol': set(),
+        'data_processing': set(),
+        'library_strategy': set(),
+        'library_source': set(),
+        'supplementary_data': set()
+    }
+
+    for sample in samples:
+        sample_data['organism'].add(find_text('.//ns:Channel/ns:Organism', sample))
+        sample_data['treatment'].add(find_text('.//ns:Channel/ns:Characteristics[@tag="treatment"]', sample))
+        sample_data['treatment_protocol'].add(find_text('.//ns:Channel/ns:Treatment-Protocol', sample))
+        sample_data['source'].add(find_text('.//ns:Channel/ns:Source', sample))
+        sample_data['characteristics'].update(char.text.strip() for char in sample.findall('.//ns:Channel/ns:Characteristics', namespaces=namespace) if char.text)
+        sample_data['molecule'].add(find_text('.//ns:Channel/ns:Molecule', sample))
+        sample_data['extract_protocol'].add(find_text('.//ns:Channel/ns:Extract-Protocol', sample))
+        sample_data['data_processing'].add(find_text('.//ns:Data-Processing', sample))
+        sample_data['library_strategy'].add(find_text('.//ns:Library-Strategy', sample))
+        sample_data['library_source'].add(find_text('.//ns:Library-Source', sample))
+        sample_data['supplementary_data'].update(supp.text.strip() for supp in sample.findall('.//ns:Supplementary-Data', namespaces=namespace) if supp.text)
+
     return {
         'series_id': series.attrib['iid'] if series is not None else '',
         'title': find_text('.//ns:Series/ns:Title'),
         'summary': find_text('.//ns:Series/ns:Summary'),
         'overall_design': find_text('.//ns:Series/ns:Overall-Design'),
-        'organism': find_text('.//ns:Sample/ns:Channel/ns:Organism'),
-        'treatment': find_text('.//ns:Sample/ns:Channel/ns:Characteristics[@tag="treatment"]'),
-        'treatment_protocol': find_text('.//ns:Sample/ns:Channel/ns:Treatment-Protocol'),
-        'source': find_text('.//ns:Sample/ns:Channel/ns:Source'),
-        'characteristics': '; '.join([char.text.strip() for char in root.findall('.//ns:Sample/ns:Channel/ns:Characteristics', namespaces=namespace)]),
-        'molecule': find_text('.//ns:Sample/ns:Channel/ns:Molecule'),
-        'extract_protocol': find_text('.//ns:Sample/ns:Channel/ns:Extract-Protocol'),
-        'data_processing': find_text('.//ns:Sample/ns:Data-Processing'),
-        'library_strategy': find_text('.//ns:Sample/ns:Library-Strategy'),
-        'library_source': find_text('.//ns:Sample/ns:Library-Source'),
-        'supplementary_data': '; '.join([supp.text.strip() for supp in root.findall('.//ns:Sample/ns:Supplementary-Data', namespaces=namespace)])
+        'organism': '; '.join(filter(None, sample_data['organism'])),
+        'treatment': '; '.join(filter(None, sample_data['treatment'])),
+        'treatment_protocol': '; '.join(filter(None, sample_data['treatment_protocol'])),
+        'source': '; '.join(filter(None, sample_data['source'])),
+        'characteristics': '; '.join(filter(None, sample_data['characteristics'])),
+        'molecule': '; '.join(filter(None, sample_data['molecule'])),
+        'extract_protocol': '; '.join(filter(None, sample_data['extract_protocol'])),
+        'data_processing': '; '.join(filter(None, sample_data['data_processing'])),
+        'library_strategy': '; '.join(filter(None, sample_data['library_strategy'])),
+        'library_source': '; '.join(filter(None, sample_data['library_source'])),
+        'supplementary_data': '; '.join(filter(None, sample_data['supplementary_data']))
     }
+
 
 # Process XML files
 xml_dir = 'data/GSE_meta'
@@ -73,8 +103,8 @@ for filename in os.listdir(xml_dir):
 
 # Verify the data
 result = con.execute("SELECT * FROM gse_metadata").fetchall()
-for row in result:
-    print(row)
+#for row in result:
+#    print(row)#
 
 # Close the connection
 con.close()
